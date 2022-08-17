b88ba7c7a87a60b046004742ace86c1ea1de24c1
Bennu-Li <yunmei.li@zilliz.com>
2022-08-17 10:12:11 +0800

    init
    Signed-off-by: Bennu-Li <yunmei.li@zilliz.com>

--- env.sh
+++
@@ -1,3 +0,0 @@
-export Chatbot="https://open.feishu.cn/open-apis/bot/v2/hook/bc3b6ade-2f1f-4c88-a091-26f0e14407fe"
-export PrometheusAddress="http://10.12.0.6:8481/select/1/prometheus"
-export NotificationServer="http://127.0.0.1:19093/api/v2/notifications"
